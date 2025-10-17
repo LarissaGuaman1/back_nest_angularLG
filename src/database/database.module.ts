57bc3a4dbe13346f3216eb4b9a853584b87d43e5
LarissaGuaman1 <lds.guaman@yavirac.ecu.ec>
2025-10-17 08:32:09 -0500

    database provider

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { databaseProviders } from './database.providers';
+import { ConfigService } from 'src/config/config.service';
 @Module({
-    providers:[...databaseProviders]
+    providers:[...databaseProviders, ConfigService],
+    exports:[...databaseProviders]
 })
 export class DatabaseModule {}
