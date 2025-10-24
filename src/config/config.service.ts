2c3f8bd9f96b0ec47f4a824d0e2cf61380228771
ElZero002 <jarystian@gmail.com>
2025-10-24 08:52:33 -0500

    creacion de tablas

---
+++ src/config/config.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import * as fs from 'fs'
+import { parse } from 'dotenv';
+
+@Injectable()
+export class ConfigService {
+    private readonly envConfig: {[key:string]:string}
+    constructor(){
+        const env =process.env.NODE_ENV || 'development'
+        const envieFilePath= `${__dirname}/../../../.env.${env}`
+        const existsPath = fs.existsSync(envieFilePath)
+            if(!existsPath){
+                console.log(`.env.${process.env.NODE_ENV} no existe`)
+                process.exit(0)
+            }
+            this.envConfig=parse(fs.readFileSync(envieFilePath))
+        }
+    get(key: string):string{
+        return this.envConfig[key];
+    }
+}
